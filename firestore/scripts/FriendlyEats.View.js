4013b8c02966a955024c246da6a44e12dc32e895
Paco Avila <pacoavila808@gmail.com>
2020-03-04 12:53:32 -0800

    rerender results on user change

--- firestore/scripts/FriendlyEats.View.js
+++ firestore/scripts/FriendlyEats.View.js
@@ -62,11 +62,13 @@ FriendlyEats.prototype.viewList = function(filters, filter_description) {
   headerEl.querySelector('#sign-in').addEventListener('click', function() {
     console.log("we doing this...");
     var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
       // The signed-in user info.
       var user = result.user;
+      that.rerender();
     }).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
